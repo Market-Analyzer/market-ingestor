562c08606261f34e725dd74a23d2b9cb62f842b2
SpenyT <spencer.toupin@outlook.com>
2026-09-09 21:57:40 -0400

    refactor(core): noexcept strong type members, drop explicit default ctor

--- Include/market_ingestor/core/strong_types.hpp
+++ Include/market_ingestor/core/strong_types.hpp
@@ -5,49 +5,49 @@
 #include <compare>  // IWYU pragma: keep
 #include <concepts>
 
-namespace market_ingestor::core 
+namespace market_ingestor::core
 {
   template<typename Tag, std::integral T = std::uint64_t>
   class ArithmeticType
   {
   public:
-    explicit constexpr ArithmeticType()    : value_{}  {}
-    explicit constexpr ArithmeticType(T v) : value_(v) {}
-    explicit constexpr operator T() const { return value_; }
+    constexpr ArithmeticType() noexcept : value_{} {}
+    explicit constexpr ArithmeticType(T v) noexcept : value_(v) {}
 
+    explicit constexpr operator T() const noexcept { return value_; }
     constexpr auto operator<=>(const ArithmeticType&) const = default;
-    constexpr ArithmeticType& operator+=(const ArithmeticType& rhs) { value_ += rhs.value_; return *this; }
-    constexpr ArithmeticType& operator-=(const ArithmeticType& rhs) { value_ -= rhs.value_; return *this; }
+    constexpr ArithmeticType& operator+=(const ArithmeticType& rhs) noexcept { value_ += rhs.value_; return *this; }
+    constexpr ArithmeticType& operator-=(const ArithmeticType& rhs) noexcept { value_ -= rhs.value_; return *this; }
 
-    [[nodiscard]] friend constexpr ArithmeticType operator+(ArithmeticType lhs, const ArithmeticType& rhs)
+    [[nodiscard]] friend constexpr ArithmeticType operator+(ArithmeticType lhs, const ArithmeticType& rhs) noexcept
     {
       lhs+=rhs;
       return lhs;
     }
 
-    [[nodiscard]] friend constexpr ArithmeticType operator-(ArithmeticType lhs, const ArithmeticType& rhs)
+    [[nodiscard]] friend constexpr ArithmeticType operator-(ArithmeticType lhs, const ArithmeticType& rhs) noexcept
     {
       lhs-=rhs;
       return lhs;
     }
-    
+
   private:
     T value_;
   };
 
-  
+
   template<typename Tag, std::integral T = std::uint64_t, T SentinelValue = T{0}>
   class DiscreteType
   {
   public:
-    explicit constexpr DiscreteType(T v) : value_(v) {}
-    explicit constexpr operator T() const { return value_; }
+    explicit constexpr DiscreteType(T v) noexcept : value_(v) {}
+    explicit constexpr operator T() const noexcept { return value_; }
 
-    [[nodiscard]] static constexpr DiscreteType sentinel() { return DiscreteType{SentinelValue}; }
-    [[nodiscard]] constexpr bool has_value() const { return value_ != SentinelValue; }
+    [[nodiscard]] static constexpr DiscreteType sentinel() noexcept { return DiscreteType{SentinelValue}; }
+    [[nodiscard]] constexpr bool has_value() const noexcept { return value_ != SentinelValue; }
 
     constexpr auto operator<=>(const DiscreteType&) const = default;
-    
+
   private:
     T value_;
   };
